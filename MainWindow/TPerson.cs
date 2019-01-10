add12c9e97c987c04683198c89b972f37d47f285
FilaCo <dvchizha@edu.hse.ru>
2019-01-10 14:03:34 +0500

    Добавлена возможность менять пол. Изменены методы set для всех свойства, для того, чтобы вызывать события. Добавлен вызов события для удаления и добавление друзей

--- MainWindow/TPerson.cs
+++ MainWindow/TPerson.cs
@@ -8,33 +8,99 @@ using System.Drawing;
 namespace MainWindow
 {
     enum TGender { MAN, WOMAN }
-    enum TStatus { NOT_CHOSEN, NOT_MARRIED, ENGAGED, MARRIED, IN_LOVE, COMPLICATED}
+    enum TStatus { NOT_CHOSEN, NOT_MARRIED, ENGAGED, MARRIED, IN_LOVE, COMPLICATED }
     delegate void PersonHandler(object source, PersonHandlerEventArgs args);
     class TPerson
-    { 
+    {
         private TJournal updates;
         public event PersonHandler PersonCharacteristicsChanged;
         public event PersonHandler PersonActionMade;
         public SortedSet<TPerson> Friends;
         public List<string> News;
         public List<string> Pictures;
-        public string FullName { get; set;}
-        public DateTime DateOfBirth { get; set; }
-        public TGender Gender { get; set; }
-        public TStatus MaritalStatus { get; set; }
-        public string School { get; set; }
-        public string HighSchool { get; set; }
+        public string FullName
+        {
+            get { return FullName; }
+            set
+            {
+                PersonHandlerEventArgs _args = new PersonHandlerEventArgs(FullName, TMessage.FULLNAME_CHANGED, DateTime.Now,
+                    $"Имя изменено на {value}");
+                FullName = value;
+                OnPersonCharacteristicsChanged(this, _args);
+            }
+        }
+        public DateTime DateOfBirth
+        {
+            get { return DateOfBirth; }
+            set
+            {
+                PersonHandlerEventArgs _args = new PersonHandlerEventArgs(FullName, TMessage.DATEOFBIRTH_CHANGED, DateTime.Now,
+                    $"Дата рождения изменена на {value}");
+                DateOfBirth = value;
+                OnPersonCharacteristicsChanged(this, _args);
+            }
+        }
+        public TGender Gender
+        {
+            get { return Gender; }
+            set
+            {
+                PersonHandlerEventArgs _args = new PersonHandlerEventArgs(FullName, TMessage.GENDER_CHANGED, DateTime.Now,
+                    $"Пол изменен на {value}");
+                Gender = value;
+                OnPersonCharacteristicsChanged(this, _args);
+            }
+        }
+        public TStatus MaritalStatus
+        {
+            get { return MaritalStatus; }
+            set
+            {
+                PersonHandlerEventArgs _args = new PersonHandlerEventArgs(FullName, TMessage.STATUS_CHANGED, DateTime.Now,
+                    $"Семейное положение изменено на {value}");
+                MaritalStatus = value;
+                OnPersonCharacteristicsChanged(this, _args);
+            }
+        }
+        public string School
+        {
+            get { return School; }
+            set
+            {
+                PersonHandlerEventArgs _args = new PersonHandlerEventArgs(FullName, TMessage.SCHOOL_CHANGED, DateTime.Now,
+                    $"Школа изменена на {value}");
+                School = value;
+                OnPersonCharacteristicsChanged(this, _args);
+            }
+        }
+        public string HighSchool
+        {
+            get { return HighSchool; }
+            set
+            {
+                PersonHandlerEventArgs _args = new PersonHandlerEventArgs(FullName, TMessage.HIGHSCHOOL_CHANGED, DateTime.Now,
+                    $"Вуз изменен на {value}");
+                HighSchool = value;
+                OnPersonCharacteristicsChanged(this, _args);
+            }
+        }
         public void AddFriend(TPerson _person)
         {
             if (Friends.Contains(_person))
                 throw new Exception($"{_person.FullName} уже есть в списке друзей");
+            PersonHandlerEventArgs _args = new PersonHandlerEventArgs(FullName, TMessage.FRIEND_ADDED, DateTime.Now,
+                    $"Добавлен новый друг {_person.FullName}");
             Friends.Add(_person);
+            OnPersonActionMade(this, _args);
         }
         public void RemoveFriend(TPerson _person)
         {
             if (!Friends.Contains(_person))
                 throw new Exception($"{_person.FullName} отсутствует в списке друзей");
+            PersonHandlerEventArgs _args = new PersonHandlerEventArgs(FullName, TMessage.FRIEND_ADDED, DateTime.Now,
+        $"Удалён друг {_person.FullName}");
             Friends.Remove(_person);
+            OnPersonActionMade(this, _args);
         }
         public void AddNews(string txt)
         {
@@ -58,19 +124,16 @@ namespace MainWindow
         }
         public override string ToString()
         {
-            return FullName;
+            return string.Format($"ФИО: {FullName};\nДата рождения: {DateOfBirth};\nПол: {Gender};\nСемейное положение: " +
+                $"{MaritalStatus};\nШкола: {School};\nВуз: {HighSchool}.");
         }
         public void OnPersonCharacteristicsChanged(object source, PersonHandlerEventArgs args)
         {
-            if (PersonCharacteristicsChanged == null)
-                throw new Exception("На событие не было подписано ни одного действия");
-            PersonCharacteristicsChanged(source, args);
+            PersonCharacteristicsChanged?.Invoke(source, args);
         }
         public void OnPersonActionMade(object source, PersonHandlerEventArgs args)
         {
-            if (PersonActionMade == null)
-                throw new Exception("На событие не было подписано ни одного действия");
-            PersonActionMade(source, args);
+            PersonActionMade?.Invoke(source, args);
         }
     }
 }
